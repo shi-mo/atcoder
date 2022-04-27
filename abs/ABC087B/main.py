fdad6ca73a2275dc8789fc31c656e0feb6c4820b
Yoshifumi Shimono <yoshifumi.shimono@gmail.com>
2022-04-27 20:49:51 +0900

    abs ABC087B: simplify algorithm.

--- abs/ABC087B/main.py
+++ abs/ABC087B/main.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python3
 import sys
 
-
 def solve(A: int, B: int, C: int, X: int):
     y = int(X/50)
 
     c = 0
     amax = min([int(y/10), A])
-    amin = max([int((y - 2*B - C + 9)/10), 0])
-    for a in reversed(range(amin, amax+1)):
+    for a in reversed(range(0, amax+1)):
         z = int(y - 10*a)
         bmax = min([int(z/2), B])
-        bmin = max([int((z-C) / 2), 0])
-        for b in reversed(range(bmin, bmax+1)):
-            c += 1 if ((z - 2*b) <= C) else 0
+        for b in reversed(range(0, bmax+1)):
+            if ((z - 2*b) <= C):
+                c += 1
     print(c)
     return
 
