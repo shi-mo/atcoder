c3797522353809be57e170bd73efe7c7c7a901f9
Yoshifumi Shimono <yoshifumi.shimono@gmail.com>
2022-04-27 20:48:09 +0900

    abs ABC088B

---
+++ abs/ABC088B/main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import sys
+
+def solve(N: int, A: "List[int]"):
+    list.sort(A, reverse=True)
+    asum = sum(A[::2])
+    bsum = sum(A[1::2])
+    print(asum - bsum)
+    return
+
+def main():
+    def iterate_tokens():
+        for line in sys.stdin:
+            for word in line.split():
+                yield word
+    tokens = iterate_tokens()
+    N = int(next(tokens))  # type: int
+    A = [int(next(tokens)) for _ in range(N)]  # type: "List[int]"
+    solve(N, A)
+
+if __name__ == '__main__':
+    main()
