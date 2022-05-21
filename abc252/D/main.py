74e08f74a34e8ba3bd414780019665169b046df4
Yoshifumi Shimono <yoshifumi.shimono@gmail.com>
2022-05-21 23:16:44 +0900

    ABC252 D

---
+++ abc252/D/main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import sys
+from collections import defaultdict
+
+def solve(N: int, A: "List[int]"):
+    if len(set(A)) <= 2: return 0
+
+    count_of = defaultdict(lambda: 0)
+    for ai in A:
+        count_of[ai] += 1
+
+    ans = N*(N-1)*(N-2)//6
+    for c in count_of.values():
+        if c < 2: continue
+        ans -= (N-c)*c*(c-1)//2
+        if 3 <= c: ans -= c*(c-1)*(c-2)//6
+    return ans
+
+def main():
+    def iterate_tokens():
+        for line in sys.stdin:
+            for word in line.split():
+                yield word
+    tokens = iterate_tokens()
+    N = int(next(tokens))  # type: int
+    A = [int(next(tokens)) for _ in range(N)]  # type: "List[int]"
+    print(solve(N, A))
+
+main()
